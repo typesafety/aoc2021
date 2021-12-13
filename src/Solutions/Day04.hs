9ea45897864867d580b86e76e9f9f12f87a0bc3d
Thomas Li <ailoodee@gmail.com>
2021-12-13 02:32:18 +0100

    Day 04: parsing works

--- src/Solutions/Day04.hs
+++ src/Solutions/Day04.hs
@@ -3,13 +3,13 @@ module Solutions.Day04
 
 import CustomPrelude
 
+import Data.IntSet qualified as IS
 import Data.Sequence qualified as Seq
-import Data.Vector.Unboxed qualified as VU
 import Data.Vector qualified as V
+import Data.Vector.Unboxed qualified as VU
 import Text.Megaparsec qualified as P
 import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as Lex
-import Text.Megaparsec.Debug
 
 
 -- * Part 1
@@ -17,38 +17,51 @@ import Text.Megaparsec.Debug
 solveP1 :: Text -> Int
 solveP1 = undefined
 
-type Board = V.Vector (VU.Vector Int)
+boardLines :: Board -> Seq IntSet
+boardLines = uncurry (Seq.><) . (&&&) rowSets colSets
+  where
+    rowSets :: Board -> Seq IntSet
+    rowSets = V.foldl' (\sq v -> vecToIs v :<| sq) []
 
+    colSets :: Board -> Seq IntSet
+    colSets = foldlCols (\is n -> IS.insert n is) []
+
+    foldlCols :: (acc -> Int -> acc) -> acc -> Board -> Seq acc
+    foldlCols f start b =
+      let width = VU.length (b V.! 0)
+      in V.foldl' (\sq colIx -> foldlCol f start colIx b :<| sq) [] [0 .. width - 1]
+
+    foldlCol :: (acc -> Int -> acc) -> acc -> Int -> Board -> acc
+    foldlCol f start colIx = V.foldl' f start . V.map (\uv -> uv VU.! colIx)
+
+    vecToIs :: VU.Vector Int -> IntSet
+    vecToIs = VU.foldl' (\is n -> IS.insert n is) []
+
+type Board = V.Vector (VU.Vector Int)
 type Parser = P.Parsec Void Text
 
-partialParseInput :: Text -> (Seq Int, Seq Board)
-partialParseInput = fromMaybe (error "Day4: bad parse") . P.parseMaybe pInput
-
-pInput :: Parser (Seq Int, Seq Board)
-pInput = do
-  ns <- pNumbers
-  void . P.some $ P.newline
-  bs <- pBoards
+bingoP :: Parser (Seq Int, Seq Board)
+bingoP = do
+  ns <- numbersP
+  _  <- P.some P.newline
+  bs <- boardsP
+  _  <- P.many P.spaceChar
   pure (ns, bs)
+  where
+    numbersP :: Parser (Seq Int)
+    numbersP = fromList <$> P.sepBy Lex.decimal (P.char ',')
 
-pNumbers :: Parser (Seq Int)
-pNumbers = fromList <$> P.sepBy Lex.decimal (P.char ',')
+    boardsP :: Parser (Seq Board)
+    boardsP = fromList <$> P.sepEndBy1 boardP P.newline
 
-pBoards :: Parser (Seq Board)
-pBoards = dbg "pBoards" $ do
-  bs <- P.sepBy pBoard (P.some P.newline)
-  pure $ fromList bs
+    boardP :: Parser Board
+    boardP = fromList <$> P.sepEndBy1 rowP P.newline
 
-pBoard :: Parser Board
-pBoard = dbg "pBoard" $ do
-  b <- P.sepBy1 (P.try pRow) P.newline
-  pure $ fromList b
-
-pRow :: Parser (VU.Vector Int)
-pRow = dbg "pRow" $ do
-  void $ P.many P.spaceChar
-  r <- P.sepBy1 Lex.decimal (P.some $ P.char ' ')
-  pure $ fromList r
+    rowP :: Parser (VU.Vector Int)
+    rowP = do
+      void $ P.many (P.char ' ')
+      r <- P.sepBy1 Lex.decimal (P.some $ P.char ' ')
+      pure $ fromList r
 
 -- * Part 2
 
